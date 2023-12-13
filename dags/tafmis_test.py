26a4f4f8495535b4e30438ca88908fa97fa33be3
tom.walton <tom.walton@digital.mod.uk>
2023-12-13 14:02:26 +0000

    Reset to standard task version

--- dags/tafmis_test.py
+++ dags/tafmis_test.py
@@ -5,8 +5,6 @@ import pendulum
 
 from airflow.decorators import dag, task
 
-from commontasks import get_auth_token
-
 
 @dag(
     schedule=None,
@@ -16,19 +14,29 @@ from commontasks import get_auth_token
 )
 def tafmis_test_data_fetch() -> dict:
 
+    
+    @task(multiple_outputs=True)
+    def get_token():
+        token_url = 'https://api.ahe.r.mil.uk/connect/token'
+        client_id = '1a23da3b-a7ac-4956-a1be-aafc31982a4f'
+        client_secret = '8446d903-f8f7-4d74-91bd-4453ae83395e'
+        data_creds = {'grant_type': 'client_credentials', 'scope': 'Mil-IndividualTraining_Access'}
+        access_token_response = requests.post(token_url,
+                                           data=data_creds,
+                                           verify=False,
+                                           allow_redirects=False,
+                                           auth=(client_id, client_secret))
+        token = json.loads(access_token_response.text)
+        return token
+    
+    
     @task()
     def load(token: dict) -> None:
 
         print(f"{token}")
 
 
-    token = get_auth_token(
-        'https://api.ahe.r.mil.uk/connect/token',
-        '1a23da3b-a7ac-4956-a1be-aafc31982a4f'
-        '8446d903-f8f7-4d74-91bd-4453ae83395e',
-        {'grant_type': 'client_credentials', 'scope': 'Mil-IndividualTraining_Access'}
-    )
-
+    token = get_token()
     load(token)
 
 
